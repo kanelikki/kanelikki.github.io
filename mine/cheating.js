f4348ce61e47331fc4c17e09acf71eab0d04ebf8
Lumi <5599742+rnielikki@users.noreply.github.com>
2019-01-03 12:49:19 +0200

    Well......

---
+++ mine/cheating.js
@@ -0,0 +1,13 @@
+﻿function hint(x=Math.floor(Math.random()*sizey),y=Math.floor(Math.random()*sizex)){
+	var elem=document.getElementById("p"+x+"x"+y);
+	if(!elem) return;
+	if(mindex.find(a=>a[0]==x&&a[1]==y)!==undefined){ mark(null,x,y,elem); }
+	else{ clicking(null,x,y,elem); }
+}
+function cheat(){//are you serious?
+	for(let i=0;i<sizey;i++){
+		for(let j=0;j<sizex;j++){
+			hint(i,j);
+		}
+	}
+}
