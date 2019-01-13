3602bc82324e5c1318aa1f652f0b33b1a36abe4d
Lumi <5599742+rnielikki@users.noreply.github.com>
2019-01-13 17:26:07 +0200

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,19 +1,22 @@
 'use strict';
 fetch('https://api.github.com/repos/rnielikki/rnielikki.github.io/contents').then((response)=>response.json()).then(function(r){
 	var menu=r.filter(x=>x.type==="dir").map(x=>x.path).filter(x=>x[0]!=='.');
-	while(document.readyState!=="complete"){}
-	//just for making sure. window.onload not works if "already" loaded
-	(function(){
+	if(document.readyState!=="complete"){
+		window.onload=setMenu(menu);
+	}
+	else setMenu(menu);
+});
+function setMenu(menu){
 	var nav=document.getElementById("nav");
 	var frag=document.createDocumentFragment();
 	for(let m in menu){
 		let txt=menu[m];
+		txt=txt[0].toUpperCase()+txt.slice(1);
 		let a=document.createElement("a");
-		a.innerText=txt[0].toUpperCase()+txt.slice(1);
+		a.innerText=txt;
 		a.href='./'+txt;
 		frag.append(a);
 	}
 	nav.innerText="";
 	nav.append(frag);
-	})();
-});
+}
