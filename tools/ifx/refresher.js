aad5f2a3ce5b538030ecf9c98cdea17c01c2d43b
Mielikki <5599742+rnielikki@users.noreply.github.com>
2025-06-27 21:58:22 +0300

    [ifx sandbox] enabled refresher for asking before leaving

--- tools/ifx/refresher.js
+++ tools/ifx/refresher.js
@@ -1,13 +1,11 @@
 const Refresher = (function () {
     let dirty = false;
-    /*
     window.addEventListener("load", function () {
         window.addEventListener("beforeunload", function (e) {
             if (!dirty) return;
             e.preventDefault();
         });
     });
-    */
     return {
         markAsDirty: function () {
             dirty = true;
